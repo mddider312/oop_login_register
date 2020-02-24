497de734008f1c1948529c2e224c35ed1556158d
Mohammed Dider <dider@innovationit.com.bd>
2020-02-25 00:06:04 +0600

    index page design complete

---
+++ index.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>OOP Login Registration</title>
+    <link rel="stylesheet" type="text/css" href="inc/bootstrap.min.css" />
+    <script type="text/javascript" src="inc/jquery.js"></script>
+    <script type="text/javascript" src="inc/bootstrap.min.js"></script>
+</head>
+
+<body>
+
+    <div class="container">
+
+        <nav class="navbar navbar-expand-sm bg-primary navbar-dark">
+            <div class="container-fluid">
+                <div class="navbar-header">
+                    <a class="navbar-brand" href="">OOP Login Registration</a>
+                </div>
+
+                <ul class="nav navbar-nav pull-right">
+                    <li class="nav-item active">
+                        <a class="nav-link" href="profile.php">Profile</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="">Logout</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="login.php">Login</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="register.php">Register</a>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+
+        <div class="card">
+            <table class="table table-striped">
+                <thead>
+                    <tr>
+                        <th scope="col">#</th>
+                        <th scope="col">First</th>
+                        <th scope="col">Last</th>
+                        <th scope="col">Handle</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <th scope="row">1</th>
+                        <td>Mark</td>
+                        <td>Otto</td>
+                        <td>@mdo</td>
+                    </tr>
+                   
+                </tbody>
+            </table>
+        </div>
+
+        <div class="card card-body bg-primary">
+            <p class="text-center">Copyrigt @ 2020</p>
+        </div>
+
+    </div>
+
+</body>
+
+</html>
--- lib/User.php
+++ lib/User.php
@@ -1,12 +1,15 @@
 <?php
 
-include_once 'Database.php';
-include 'Session.php';
+include_once 'Session.php';
+include 'Database.php';
 
 class User{
     
+    private db;
+
     public function __construct(){
-        
+    	
+    	$this->db = new Database();    
     }
 }
 ?>
